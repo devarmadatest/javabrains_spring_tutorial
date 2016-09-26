44e7119e24da8bb1624dea5b9d63cfb581542152
Andrea De Rinaldis <andrea@derinaldis.com>
2016-09-26 21:12:01 +0200

    lesson 05 completed

--- src/main/java/com/derinaldis/javabrainsspringtutorial/Triangle.java
+++ src/main/java/com/derinaldis/javabrainsspringtutorial/Triangle.java
@@ -1,7 +1,19 @@
 package com.derinaldis.javabrainsspringtutorial;
 
-public class Triangle{
-  public void draw(){
-    System.out.println("Triangle.draw");
-  }
+public class Triangle {
+
+private String type;
+
+public void setType(String type){
+        this.type=type;
+}
+
+public String getType(){
+        return type;
+}
+
+public void draw(){
+        System.out.println(String.format("%s triangle.draw", getType()));
+}
+
 }
