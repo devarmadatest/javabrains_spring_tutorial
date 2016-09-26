1e79ff663b3d6682ba197c07e3cf263082b8f965
Andrea De Rinaldis <andrea@derinaldis.com>
2016-09-26 21:31:00 +0200

    lesson 06 completed

--- src/main/java/com/derinaldis/javabrainsspringtutorial/Triangle.java
+++ src/main/java/com/derinaldis/javabrainsspringtutorial/Triangle.java
@@ -3,17 +3,31 @@ package com.derinaldis.javabrainsspringtutorial;
 public class Triangle {
 
 private String type;
+private int height;
 
-public void setType(String type){
+public Triangle(String type){
         this.type=type;
+}
+
+public Triangle(int height){
+        this.height=height;
+}
+
+public Triangle(String type, int height){
+        this.type=type;
+        this.height=height;
 }
 
 public String getType(){
         return type;
 }
 
+public int getHeight(){
+        return height;
+}
+
 public void draw(){
-        System.out.println(String.format("%s triangle.draw", getType()));
+        System.out.println(String.format("%s triangle.draw of height %s", getType(), getHeight()));
 }
 
 }
